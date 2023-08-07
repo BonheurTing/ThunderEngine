420dcaa5165a3e37ee95c5a6446694c3bb8ea127
BonheurTing <3281729978@qq.com>
2023-08-08 00:42:49 +0800

    shader complile func

---
+++ Source/Runtime/Shader/Private/ShaderCompiler.cpp
@@ -0,0 +1,11 @@
+﻿#include "ShaderCompiler.h"
+
+void IDx11Compiler::Compile(const String& inSource, BinaryData& outByteCode)
+{
+	
+}
+
+void IDx12Compiler::Compile(const String& inSource, BinaryData& outByteCode)
+{
+	
+}
---
+++ Source/Runtime/Shader/Public/ShaderCompiler.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+
+class Shader_API ICompiler
+{
+public:
+	virtual void Compile(const String& inSource, BinaryData& outByteCode) = 0;
+};
+
+class Shader_API IDx11Compiler : public ICompiler
+{
+public:
+	void Compile(const String& inSource, BinaryData& outByteCode) override;
+};
+
+class Shader_API IDx12Compiler : public ICompiler
+{
+public:
+	void Compile(const String& inSource, BinaryData& outByteCode) override;
+};

06e4cb313dd110728391c00915d3cd8c75c1458c
BonheurTing <3281729978@qq.com>
2025-04-05 23:41:38 +0800

    语法树逻辑移到头文件；批处理增加编译文件测试

--- Source/Runtime/ShaderLang/Private/compile.bat
+++ Source/Runtime/ShaderLang/Private/compile.bat
@@ -7,3 +7,13 @@ cd %tool_dir%
 .\bison.exe --yacc -dv --output=%current_dir%\output\y.tab.c --defines=%current_dir%\output\y.tab.h %current_dir%\%str%.y
 .\flex.exe -o%current_dir%\output\lex.yy.c %current_dir%\%str%.l
 gcc -o %current_dir%\%str% %current_dir%\output\y.tab.c %current_dir%\output\lex.yy.c
+
+if %errorlevel% neq 0 (
+    echo Compilation failed! Please check the code for errors.
+    pause
+    exit /b %errorlevel%
+)
+
+echo Compilation succeeded. Shader file is being compiled...
+cd %current_dir%
+%str%.exe -f example.tsf
